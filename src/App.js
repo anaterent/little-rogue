353399e2be98e25ec0d628b26038a60855c868a9
apricot <just.anastasia.terenteva@gmail.com>
2023-12-02 18:11:10 +1100

    turn css into sass file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.sass';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
